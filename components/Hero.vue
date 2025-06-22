92e0a695379ad75df05a78d4d6c842a767cd21b7
Samnoon Haider <haider@Mac.attlocal.net>
2025-06-22 14:07:19 -0400

    Enhance responsiveness

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="h-[90vh] bg-gradient-to-br from-gray-900 to-gray-600 flex justify-center bg-red mb-4">
-        <div class="flex items-center justify-center gap-6 flex-col md:flex-row">
-            <img src="/images/profile.png" class="w-[100%] md:w-[50%]" />
-            <div class="flex items-center justify-center gap-2 flex-col w-[100%] md:w-[50%]">
+    <div class="h-[90vh] bg-gradient-to-b from-black to-gray-800 flex justify-center bg-red mb-4">
+        <div class="flex items-center justify-center gap-6 flex-col lg:flex-row">
+            <div class="w-[100%] lg:w-[50%] max-w-[600px] p-8">
+                <img src="/images/profile.png" class="rounded-[50%] border border-4 border-white" />
+            </div>
+            <div class="flex items-center justify-center gap-2 flex-col w-[100%] lg:w-[50%] text-center">
                 <div class="text-[50px] lg:text-[90px] font-bold">Sam Haider</div>
-                <div class="text-[30px] lg:text-[50px] font-bold text-center w-[100%] md:w-[50%] bg-gradient-to-r from-green-500 to-blue-500 bg-clip-text text-transparent">
+                <div class="text-[30px] lg:text-[50px] font-bold text-center w-[100%] lg:w-[50%] bg-gradient-to-r from-green-500 to-blue-500 bg-clip-text text-transparent">
                     Software Engineer
                 </div>
             </div>
