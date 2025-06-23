a175df242f4e216419763be0a7d810bb4a798b43
Samnoon Haider <haider@Mac.attlocal.net>
2025-06-23 11:34:08 -0400

    Add gradient bg

--- components/Skills.vue
+++ components/Skills.vue
@@ -5,7 +5,7 @@
                 <h2 class="text-4xl font-semibold pb-8">Top Skills</h2>
             </div>
             <div class="grid gap-4 grid-cols-[repeat(auto-fit,minmax(100px,1fr))]">
-                <div v-for="skill in skills" class="border border-1 border-white rounded-xl py-1 flex items-center justify-center bg-green-500 text-black font-bold">{{ skill }}</div>
+                <div v-for="skill in skills" class="border border-1 border-white rounded-xl py-1 flex items-center justify-center bg-gradient-to-br from-green-500 to-green-600 text-black font-bold">{{ skill }}</div>
             </div>
         </div>
     </div>
