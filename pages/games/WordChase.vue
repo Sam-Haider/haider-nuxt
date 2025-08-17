4009b8a3a8514aab20b0c0d7322042156f64e2f1
Sam Haider <sammhaider0@gmail.com>
2025-08-17 14:24:42 -0400

    animate the UI

--- pages/games/WordChase.vue
+++ pages/games/WordChase.vue
@@ -36,7 +36,12 @@
         <div
           v-for="idx in 6"
           :key="idx"
-          class="border border-2 border-teal-400 w-16 h-16 text-center transition-opacity duration-500 opacity-100 flex items-center justify-center"
+          class="border border-2 border-teal-400 w-12 h-12 text-center flex items-center justify-center transition-all duration-500"
+          :class="{
+            'opacity-100 scale-110': letters[idx - 1],
+            'opacity-50': !letters[idx - 1],
+          }"
+          :style="{ transitionDelay: `${idx * 100}ms` }"
         >
           {{ letters[idx - 1] || "" }}
         </div>
