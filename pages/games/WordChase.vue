87e8a6be66d5fd2bc1ba1f27ebe1c45784746158
Sam Haider <sammhaider0@gmail.com>
2025-08-24 16:18:55 -0400

    Update WordChase.vue

--- pages/games/WordChase.vue
+++ pages/games/WordChase.vue
@@ -6,148 +6,153 @@
 <!-- show words you captured -->
 
 <template>
-  <div
-    class="max-w-4xl mx-auto py-5 px-3 h-[100vh] flex flex-col border-1 border-red-400"
-  >
-    <!-- Heading: Title, Rules -->
-    <div class="flex justify-between w-full px-4 items-center">
-      <div class="w-16"></div>
-      <div class="text-2xl font-bold">WordChase</div>
-      <button
-        class="text-md border-1 px-3 py-1 rounded-4xl bg-gradient-to-r bg-cyan-900/20 text-white"
-        @click="toggleInstructions"
-      >
-        Rules
-      </button>
-    </div>
-
-    <!-- Game Status: Lives, Score, Word Attempt Result-->
+  <div class="p-2 bg-gradient-to-r from-cyan-200 to-teal-800">
     <div
-      v-if="!gameOver"
-      class="border-b-1 border-white-400 p-3 flex flex-col gap-3"
+      class="max-w-4xl mx-auto py-5 px-3 h-[100vh] flex flex-col border-1 bg-black"
     >
-      <div class="text-2xl flex h-12">
-        Lives:
-        <span class="time text-teal-400 inline-flex gap-4 px-3">
-          <Icon
-            v-for="life in livesRemaining"
-            name="hugeicons:brain-02"
-            class="text-teal-400 text-3xl"
-          />
-        </span>
+      <!-- Heading: Title, Rules -->
+      <div class="flex justify-between w-full px-4 items-center">
+        <div class="w-16"></div>
+        <div class="text-2xl font-bold">WordChase</div>
+        <button
+          class="text-md border-1 px-3 py-1 rounded-4xl bg-gradient-to-r bg-cyan-900/20 text-white"
+          @click="toggleInstructions"
+        >
+          Rules
+        </button>
       </div>
-      <div class="text-2xl h-12">
-        Score: <span class="score text-teal-400">{{ score }}</span>
-      </div>
-    </div>
-    <div v-else class="h-33"></div>
 
-    <!-- Game Board/Letters -->
-    <div class="gap-2 justify-center text-4xl items-center">
-      <template v-if="!gameOver">
-        <div class="flex flex-col gap-4 h-45">
-          <div class="flex justify-center gap-3 px-3 py-6">
+      <!-- Game Status: Lives, Score, Word Attempt Result-->
+      <div
+        v-if="!gameOver"
+        class="border-b-1 border-white-400 p-3 flex flex-col gap-3"
+      >
+        <div class="text-2xl flex h-12">
+          Lives:
+          <span class="time text-teal-400 inline-flex gap-4 px-3">
+            <Icon
+              v-for="life in livesRemaining"
+              name="hugeicons:brain-02"
+              class="text-teal-400 text-3xl"
+            />
+          </span>
+        </div>
+        <div class="text-2xl h-12">
+          Score: <span class="score text-teal-400">{{ score }}</span>
+        </div>
+      </div>
+      <div v-else class="h-33"></div>
+
+      <!-- Game Board/Letters -->
+      <div class="gap-2 justify-center text-4xl items-center">
+        <template v-if="!gameOver">
+          <div class="flex flex-col gap-4 h-45">
+            <div class="flex justify-center gap-3 px-3 py-6">
+              <div
+                v-for="idx in 6"
+                :key="idx"
+                class="border border-2 border-teal-400 w-16 h-16 text-center flex items-center justify-center"
+              >
+                <span
+                  class="transition-all duration-500"
+                  :class="{
+                    'opacity-0 scale-95': !letters[idx - 1],
+                    'opacity-100 scale-100': letters[idx - 1],
+                  }"
+                >
+                  {{ letters[idx - 1] || "" }}
+                </span>
+              </div>
+            </div>
             <div
-              v-for="idx in 6"
-              :key="idx"
-              class="border border-2 border-teal-400 w-16 h-16 text-center flex items-center justify-center"
+              v-if="showTargetWord"
+              class="flex justify-center items-center items-center gap-3 text-xl h-12"
             >
               <span
-                class="transition-all duration-500"
-                :class="{
-                  'opacity-0 scale-95': !letters[idx - 1],
-                  'opacity-100 scale-100': letters[idx - 1],
-                }"
+                v-if="isCorrectWord"
+                class="text-green-500 text-3xl font-bold"
               >
-                {{ letters[idx - 1] || "" }}
+                <Icon
+                  name="material-symbols:check-circle"
+                  class="text-green-500"
+                />
               </span>
+              <span
+                v-else
+                class="text-red-500 text-lg rounded-4xl px-2 border-1 border-red-500"
+                >X</span
+              ><span class="text-3xl">{{ targetWord }}</span>
+            </div>
+            <!-- empty space to avoid layout shift -->
+            <div v-else class="h-12"></div>
+          </div>
+        </template>
+        <template v-else>
+          <div class="h-45">
+            <div v-if="!firstLoad" class="text-sm flex flex-col gap-2">
+              Completed Words:
+              <div class="flex gap-2 overflow-x-auto whitespace-nowrap pb-3">
+                <div
+                  v-for="word in capturedWords"
+                  class="border border1 border-teal-400 rounded-4xl px-2"
+                >
+                  {{ word }}
+                </div>
+              </div>
+              Missed Words:
+              <div
+                class="flex gap-2 overflow-x-auto whitespace-nowrap mb-8 pb-3"
+              >
+                <div
+                  v-for="word in missedWords"
+                  class="border border1 border-red-500 rounded-4xl px-2"
+                >
+                  {{ word }}
+                </div>
+              </div>
+            </div>
+            <div class="flex gap-2 justify-center">
+              <button
+                class="text-3xl border-1 border-teal-400 px-5 py-2 rounded-4xl text-white bg-cyan-900/20"
+                @click="handlePlay"
+              >
+                PLAY {{ !firstLoad ? "AGAIN" : "" }}
+              </button>
             </div>
           </div>
-          <div
-            v-if="showTargetWord"
-            class="flex justify-center items-center items-center gap-3 text-xl h-12"
+        </template>
+      </div>
+
+      <!-- Game Status MAIN -->
+
+      <Keyboard :gameOver="gameOver" @key-press="clickHandler" class="w-full" />
+
+      <div
+        v-if="showInstructions"
+        class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
+      >
+        <div class="rounded-lg shadow-lg p-6 w-3/4 w-full h-full relative">
+          <button
+            @click="showInstructions = false"
+            class="absolute top-1 right-4 text-gray-500 hover:text-gray-700"
           >
-            <span
-              v-if="isCorrectWord"
-              class="text-green-500 text-3xl font-bold"
-            >
-              <Icon
-                name="material-symbols:check-circle"
-                class="text-green-500"
-              />
-            </span>
-            <span
-              v-else
-              class="text-red-500 text-lg rounded-4xl px-2 border-1 border-red-500"
-              >X</span
-            ><span class="text-3xl">{{ targetWord }}</span>
-          </div>
-          <!-- empty space to avoid layout shift -->
-          <div v-else class="h-12"></div>
-        </div>
-      </template>
-      <template v-else>
-        <div class="h-45">
-          <div v-if="!firstLoad" class="text-sm flex flex-col gap-2">
-            Completed Words:
-            <div class="flex gap-2 overflow-x-auto whitespace-nowrap">
-              <div
-                v-for="word in capturedWords"
-                class="border border1 border-teal-400 rounded-4xl px-2"
-              >
-                {{ word }}
-              </div>
+            <span class="text-6xl">&times;</span>
+          </button>
+          <div class="text-xl px-10 mt-4">
+            <div class="instructions">
+              <div class="text-4xl text-bold">HOW TO PLAY</div>
+              <ul class="flex flex-col gap-2 list-disc mt-8">
+                <li>
+                  Work with the computer to make as many 6-letter words as
+                  possible
+                </li>
+                <li>
+                  If you miss a word by playing the wrong letter, you lose a
+                  life
+                </li>
+                <li>Lose 3 lives and its game over</li>
+              </ul>
             </div>
-            Missed Words:
-            <div class="flex gap-2 overflow-x-auto whitespace-nowrap mb-8">
-              <div
-                v-for="word in missedWords"
-                class="border border1 border-red-500 rounded-4xl px-2"
-              >
-                {{ word }}
-              </div>
-            </div>
-          </div>
-          <div class="flex gap-2 justify-center">
-            <button
-              class="text-3xl border-1 border-teal-400 px-5 py-2 rounded-4xl text-white bg-cyan-900/20"
-              @click="handlePlay"
-            >
-              PLAY {{ !firstLoad ? "AGAIN" : "" }}
-            </button>
-          </div>
-        </div>
-      </template>
-    </div>
-
-    <!-- Game Status MAIN -->
-
-    <Keyboard :gameOver="gameOver" @key-press="clickHandler" class="w-full" />
-
-    <div
-      v-if="showInstructions"
-      class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
-    >
-      <div class="rounded-lg shadow-lg p-6 w-3/4 w-full h-full relative">
-        <button
-          @click="showInstructions = false"
-          class="absolute top-1 right-4 text-gray-500 hover:text-gray-700"
-        >
-          <span class="text-6xl">&times;</span>
-        </button>
-        <div class="text-xl px-10 mt-4">
-          <div class="instructions">
-            <div class="text-4xl text-bold">HOW TO PLAY</div>
-            <ul class="flex flex-col gap-2 list-disc mt-8">
-              <li>
-                Work with the computer to make as many 6-letter words as
-                possible
-              </li>
-              <li>
-                If you miss a word by playing the wrong letter, you lose a life
-              </li>
-              <li>Lose 3 lives and its game over</li>
-            </ul>
           </div>
         </div>
       </div>
